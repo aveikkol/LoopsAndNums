4c2039776e9ca5be7707ae3d494846fa49b07634
angelina-alice veikkolainen <aveikkol@tktk.ee>
2021-10-21 20:50:43 +0300

    Loops and nums

---
+++ GuessingGameInfinite/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GuessingGameInfinite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata, siis ta on mängu võitnud;
+            //katsete arv on piiramatu. 
+            //*programm genereerib juhuslikku numbrit ühte korda. 
+
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1, 11);
+            bool a = true;
+
+            while (a)
+            {
+                Console.WriteLine($"Sisesta number 1-10");
+                int myRandomNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (myRandomNumber == cpuNumber)
+                {
+                    Console.WriteLine("Oled võitja!");
+                    a = false;
+                }
+            }
+        }
+    }
+}
---
+++ LoopsAndRandomNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LoopsAndRandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada PIN-koodi;
+            //programm võrdleb sisestatud PIN-koodi arvuga 1234;
+            //kui sisestatud PIN-kood on 1234, siis konsool kuvab "Tere tulemsat!";
+            //kui sisestatud PIN-kood on midagi muud, siis konsool kuvab "Vale PIN. Proovi uuesti";
+            //kasutajal on kolm katset.
+
+            Console.WriteLine("Sisesta PIN:");
+            int userPIN = Convert.ToInt32(Console.ReadLine());
+
+            if(userPIN == 1234)
+            {
+                Console.WriteLine("Tere tulemst!");
+
+            }
+            else
+            {
+                Console.WriteLine("Vale PIN. Proovi uuesti");
+
+            }
+            Console.WriteLine("Kena päeva!");
+
+            
+
+
+        }
+    }
+}
